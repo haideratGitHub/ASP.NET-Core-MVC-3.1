11b2518381c8c114f8d5b02b369d6e4caa719df7
Haider <haiderabbas1982000@gmail.com>
2020-07-19 11:30:10 +0500

    13. Added multiple controllers

---
+++ BookStore/BookStore 2.0/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_2._0.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public String GetAllBooks()
+        {
+            return "All books";
+        }
+        public String GetBook(int id)
+        {
+            return $"book with id = {id}";
+        }
+    }
+}
