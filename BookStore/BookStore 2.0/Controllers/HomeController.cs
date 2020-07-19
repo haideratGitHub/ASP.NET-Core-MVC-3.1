bbef87fe3e7a2035f8f79828b64085e908afac20
Haider <haiderabbas1982000@gmail.com>
2020-07-19 11:08:54 +0500

    Added mvc to application

---
+++ BookStore/BookStore 2.0/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_2._0.Controllers
+{
+    public class HomeController:Controller
+    {
+        public String Index()
+        {
+            return "Haider";
+        }
+    }
+}
--- BookStore/BookStore 2.0/Startup.cs
+++ BookStore/BookStore 2.0/Startup.cs
@@ -16,7 +16,7 @@ namespace BookStore_2._0
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,10 +31,13 @@ namespace BookStore_2._0
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                //to use HomeController Index... add this
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
