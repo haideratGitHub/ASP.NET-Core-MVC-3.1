4b65826fccda97b8ae1921adc564ea58aa6cf893
Haider <haiderabbas1982000@gmail.com>
2020-07-24 21:45:53 +0500

    Added Migration

--- BookStore/BookStore 2.0/Data/Books.cs
+++ BookStore/BookStore 2.0/Data/Books.cs
@@ -7,5 +7,13 @@ namespace BookStore_2._0.Data
 {
     public class Books
     {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public int Totalpages { get; set; }
     }
 }
