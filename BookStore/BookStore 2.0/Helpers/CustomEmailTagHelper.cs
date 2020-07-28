6e5c80cb33f6331d9e7cebd6103c2c5c27a60a4e
Haider <haiderabbas1982000@gmail.com>
2020-07-28 17:11:39 +0500

    Added custom tag helper

---
+++ BookStore/BookStore 2.0/Helpers/CustomEmailTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_2._0.Helpers
+{
+    public class CustomEmailTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+        }
+    }
+}
