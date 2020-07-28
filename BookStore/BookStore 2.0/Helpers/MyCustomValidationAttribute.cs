ab4cd870906039c1b78cd0416f1ce00986a3dc94
Haider <haiderabbas1982000@gmail.com>
2020-07-28 17:05:15 +0500

    Added custom validations

---
+++ BookStore/BookStore 2.0/Helpers/MyCustomValidationAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_2._0.Helpers
+{
+    public class MyCustomValidationAttribute:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return base.IsValid(value, validationContext); 
+        }
+    }
+}
